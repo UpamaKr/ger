4cc02dd0b8eafd631632d81c88d9e9da9c0ff66b
UpamaKr <upama.sarker@yahoo.com>
2023-11-28 18:06:16 +0100

    digital clock added

--- ger-eng.py
+++ ger-eng.py
@@ -8,7 +8,7 @@ window.geometry("600x300")
 window.configure(bg='steelblue')
 
 
-label = Label(window, text='Hello', font=('Arial Black', 78, 'bold'), bg='steelblue', fg='white')
+label = Label(window, font=('Arial Black', 78, 'bold'), bg='steelblue', fg='white')
 label.pack(pady = 100)
 
 def clock():
