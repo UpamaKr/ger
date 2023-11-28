87c1cb8101a6bd8d2280542f5c47709cad638adb
UpamaKr <upama.sarker@yahoo.com>
2023-11-28 16:55:43 +0100

    welcome is changed to hello

--- ger-eng.py
+++ ger-eng.py
@@ -6,6 +6,6 @@ window.title('German Words')
 window.geometry("600x300")
 
 
-label = Label(window, text='Welcome', font=('Arial Black', 78, 'bold'), bg='steelblue', fg='white')
+label = Label(window, text='Hello', font=('Arial Black', 78, 'bold'), bg='steelblue', fg='white')
 label.pack(pady = 100)
 window.mainloop()
