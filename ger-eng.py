d7f0815da913fbd7536e6f94265095ed6ddcfff6
UpamaKr <upama.sarker@yahoo.com>
2023-11-28 16:24:50 +0100

    added a welcome message

--- ger-eng.py
+++ ger-eng.py
@@ -4,4 +4,8 @@ from tkinter import Tk, Label
 window = Tk()
 window.title('German Words')
 window.geometry("600x300")
+
+
+label = Label(window, text='Welcome', font=('Arial Black', 78, 'bold'), bg='steelblue', fg='white')
+label.pack(pady = 100)
 window.mainloop()
